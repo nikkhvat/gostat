0596e3e6681c30ab0d69cfd7c2458631daf9742f
Nikita Khvatov <nikhvatov19@gmail.com>
2023-07-16 17:03:45 +0600

    add env variables (for database and port)

--- auth_service/cmd/api_auth/app.go
+++ auth_service/cmd/api_auth/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -10,12 +11,21 @@ import (
 	"github.com/nik19ta/gostat/auth_service/internal/auth/model"
 	"github.com/nik19ta/gostat/auth_service/internal/auth/repository/postgres"
 	"github.com/nik19ta/gostat/auth_service/internal/auth/service"
+	"github.com/nik19ta/gostat/auth_service/pkg/env"
 	pb "github.com/nik19ta/gostat/auth_service/proto/auth"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	db, err := gorm.Open("postgres", "host=localhost user=author dbname=gostat password=123456789 sslmode=disable")
+	host := env.Get("DB_HOST")
+	user := env.Get("DB_USER")
+	dbname := env.Get("DB_NAME")
+	password := env.Get("DB_PASSWORD")
+	sslmode := env.Get("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=%s", host, user, dbname, password, sslmode)
+
+	db, err := gorm.Open("postgres", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
@@ -28,7 +38,7 @@ func main() {
 	authService := service.NewAuthService(userRepo)
 	authHandler := grpcDelivery.NewAuthServiceHandler(authService)
 
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", env.Get("PORT"))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -36,7 +46,8 @@ func main() {
 	s := grpc.NewServer()
 	pb.RegisterAuthServiceServer(s, authHandler)
 
-	log.Println("Server is running on port 50051...")
+	log.Printf("Server is running on port %s", env.Get("PORT"))
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
