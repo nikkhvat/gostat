fb0c87a23d40de356018c252fa59328690b711c2
Nikita Khvatov <nikhvatov19@gmail.com>
2023-07-18 19:41:24 +0600

    add docker for app_service

--- init.sql
+++ init.sql
@@ -5,3 +5,6 @@ GRANT ALL PRIVILEGES ON DATABASE gostat_stats_service TO author;
 
 CREATE DATABASE gostat_auth_service WITH OWNER = author;
 GRANT ALL PRIVILEGES ON DATABASE gostat_auth_service TO author;
+
+CREATE DATABASE gostat_app_service WITH OWNER = author;
+GRANT ALL PRIVILEGES ON DATABASE gostat_app_service TO author;
