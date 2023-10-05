3ac3a3f19aacafc3e08e13d4a5b34ba66e25c58f
Ali-Aliev-pr <72982603+Ali-Aliev-pr@users.noreply.github.com>
2023-10-05 19:32:59 +0400

    started to write logic on registration

--- client_service/src/app/auth/components/Input/index.tsx
+++ client_service/src/app/auth/components/Input/index.tsx
@@ -1,5 +1,9 @@
-import React, { ChangeEvent } from 'react';
+"use client"
+import React, { ChangeEvent, useEffect } from 'react';
 import styles from './index.module.css'
+import Image from 'next/image';
+
+import eye from '../../../assets/auth/eye.svg';
 
 interface InputProps {
   type: string;
@@ -9,15 +13,35 @@ interface InputProps {
 }
 
 const Input: React.FC<InputProps> = ({ type, placeholder}) => {
+
+  useEffect(() => {
+    console.log('useEffect')
+  },[])
+
+  const click = () => {
+    console.log('click')
+  }
+
   return (
     <div className={styles.container}>
+
       <input
         className={styles.input}
         type={type}
         placeholder={placeholder}
         // onChange={onChange}
       />
-      
+
+      {
+        type === "password" && 
+          <div className={styles.element} onClick={() => {console.log(12345)}}>
+            <Image 
+              src={eye}
+              alt='eye icon'
+            />
+          </div>
+      }
+
     </div>
   );
 }
