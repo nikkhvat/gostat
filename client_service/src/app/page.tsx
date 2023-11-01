51b6359274ba094c9d0819df45a3ec2a7a6ea061
Nikita Khvatov <nikhvatov19@gmail.com>
2023-11-01 23:48:47 +0400

    fix home

--- client_service/src/app/page.tsx
+++ client_service/src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+import { useEffect } from 'react';
 import styles from './page.module.css' 
 
+import Storage from "@/app/utils/storage";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Storage.get("access_token");
+
+    if (token != null && token.length > 0) {
+      router.push("/dashboard", { scroll: false });
+    } else {
+      router.push("/auth/sign-in", { scroll: false });
+    }
+  }, [router]);
+
   return (
     <main className={styles.home}>
-      <p>Home</p>
     </main>
   )
 }
