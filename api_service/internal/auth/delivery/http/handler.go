74721b9b30ff6f2e2cc32ecbde3ffde509829e41
Nikita Khvatov <nikhvatov19@gmail.com>
2023-11-02 14:57:31 +0400

    fix error message response

--- api_service/internal/auth/delivery/http/handler.go
+++ api_service/internal/auth/delivery/http/handler.go
@@ -184,22 +184,22 @@ func (h *AuthHandler) Registration(c *gin.Context) {
 	token, err := h.service.Registration(c.Request.Context(), req)
 	if err != nil {
 		if strings.Contains(err.Error(), "email already exists") {
-			c.JSON(409, ErrorAuthResponse{Error: "User with the same email already exists"})
+			c.JSON(409, ErrorAuthResponse{Error: "email already in use"})
 			return
 		}
 
 		if strings.Contains(err.Error(), "login already exists") {
-			c.JSON(409, ErrorAuthResponse{Error: "User with the same login already exists"})
+			c.JSON(409, ErrorAuthResponse{Error: "login already in use."})
 			return
 		}
 
 		if strings.Contains(err.Error(), "duplicate key value violates unique constraint \"users_email_key\"") {
-			c.JSON(409, ErrorAuthResponse{Error: "duplicate key value violates unique constraint users_email"})
+			c.JSON(409, ErrorAuthResponse{Error: "email already in use"})
 			return
 		}
 
 		if strings.Contains(err.Error(), "duplicate key value violates unique constraint \"uix_users_login\"") {
-			c.JSON(409, ErrorAuthResponse{Error: "duplicate key value violates unique constraint users_login"})
+			c.JSON(409, ErrorAuthResponse{Error: "login already in use."})
 			return
 		}
 
