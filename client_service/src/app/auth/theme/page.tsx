e86f1643994f67925f3abc56fb4b253ffdda3316
Ali-Aliev-pr <72982603+Ali-Aliev-pr@users.noreply.github.com>
2023-11-13 01:53:24 +0300

    finished theme layout

--- client_service/src/app/auth/theme/page.tsx
+++ client_service/src/app/auth/theme/page.tsx
@@ -2,6 +2,11 @@
 import React, { useState } from "react";
 import style from '@/app/auth/theme/page.module.css';
 import Link from "next/link";
+import Image from 'next/image';
+
+import lightCat from '@/app/assets/auth/light-cat.svg';
+import darkCat from '@/app/assets/auth/dark-cat.svg';
+import halfCat from '@/app/assets/auth/half-cat.svg';
 
 export default function Theme() {
   return (
@@ -12,7 +17,22 @@ export default function Theme() {
         <p className={style.description}>Select an appearance and see how the menu, buttons and windows adjust depending on which one you  choose</p>
       </div>
 
-      <div className={style.main__block}></div>
+      <div className={style.main__block}>
+        <div className={style.light__button}>
+          <Image src={lightCat} alt="light-cat" />
+        </div>
+        <div className={style.dark__button}>
+          <Image src={darkCat} alt="dark-cat" />
+        </div>
+        <div className={style.system__button}>
+          <div className={style.system__button__left}>
+            <Image src={lightCat} alt="light-cat" />
+          </div>
+          <div className={style.system__button__right}>
+            <Image src={halfCat} alt="half-cat" />
+          </div>
+        </div>
+      </div>
 
       <div className={style.box__bottom}>
         <div className={style.bottom__bottoms}>
