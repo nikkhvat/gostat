c2175d212fe91ba0d5b1d944895340fbf4d9f409
Nikita Khvatov <nikhvatov19@gmail.com>
2023-11-14 19:18:30 +0400

    fix

--- client_service/src/app/auth/language/page.tsx
+++ client_service/src/app/auth/language/page.tsx
@@ -1,44 +1,54 @@
 "use client"
 import React, { useState } from "react";
-import style from '@/app/auth/language/page.module.css';
-import Image from 'next/image';
 
-import image from '@/app/assets/auth/earth.svg';
+import style from '@/app/auth/language/page.module.css';
+
+import Image from 'next/image';
 import Link from "next/link";
 
 import LanguageButton from "@/app/auth/components/languageButton/index";
 
+import image from '@/app/assets/auth/earth.svg';
+
 export default function Language() {
+  const languages = [
+    { code: "EN", name: "English" },
+    { code: "DE", name: "Deutsch" },
+    { code: "FR", name: "Français" },
+    { code: "ES", name: "Español" },
+    { code: "RU", name: "Русский" },
+    { code: "KK", name: "қазақша" },
+    { code: "JA", name: "日本語" },
+    { code: "KO", name: "한국어" },
+    { code: "ZH", name: "中文" },
+  ]
 
-  const [languageSelected, setLanguageSelected] = useState('EN');
-
-  const changeLanguage = (language: string) => {
-    setLanguageSelected(language)
-  }
+  const [languageSelected, setLanguageSelected] = useState(languages[0].code);
+  const changeLanguage = (language: string) => setLanguageSelected(language);
 
   return (
     <div className={style.box}>
-
       <div className={style.texts}>
         <h1 className={style.title}>Welcome</h1>
-        <p className={style.subTitle}>Setting up the service will not take much time</p>
+        <p className={style.subTitle}>
+          Setting up the service will not take much time
+        </p>
       </div>
 
       <div className={style.mainBlock}>
         <p className={style.chooseLanguageTitle}>Choose your language</p>
 
         <div className={style.languageButtons}>
-          <LanguageButton article="EN" language="English" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="RU" language="Русский" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="JA" language="日本語" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="ZH" language="中文" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="DE" language="Deutsch" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="FR" language="Français" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="ES" language="Español" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="KK" language="қазақша" selected={languageSelected} changeLanguage={changeLanguage} />
-          <LanguageButton article="KO" language="한국어" selected={languageSelected} changeLanguage={changeLanguage} />
+          {languages.map((language) => (
+            <LanguageButton
+              key={language.code}
+              article={language.code}
+              language={language.name}
+              selected={languageSelected}
+              changeLanguage={changeLanguage}
+            />
+          ))}
         </div>
-
       </div>
 
       <div className={style.earthPhoto}>
@@ -47,13 +57,13 @@ export default function Language() {
       <div className={style.box__bottom}>
         <div className={style.bottom__bottoms}>
           <Link className={style.back__button} href="/dashboard">
-              Back
+            Back
           </Link>
           <Link className={style.continue__button} href="/dashboard">
-              Continue
+            Continue
           </Link>
-        </div>  
+        </div>
       </div>
     </div>
-  )
+  );
 }
