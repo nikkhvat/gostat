2009c8893ce35915f6586c976ca03432572fbd71
Ali-Aliev-pr <72982603+Ali-Aliev-pr@users.noreply.github.com>
2023-11-20 20:54:03 +0300

    finished theme layout

--- client_service/src/app/auth/theme/page.tsx
+++ client_service/src/app/auth/theme/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import style from '@/app/auth/theme/page.module.css';
 import Link from "next/link";
 import Image from 'next/image';
@@ -9,6 +9,9 @@ import darkCat from '@/app/assets/auth/dark-cat.svg';
 import halfCat from '@/app/assets/auth/half-cat.svg';
 
 export default function Theme() {
+
+  let [theme, setTheme] = useState('auto');
+
   return (
     <div className={style.box}>
 
@@ -19,28 +22,40 @@ export default function Theme() {
 
       <div className={style.main__block}>
 
-        <div>
-          <div className={style.light__button}>
-            <Image src={lightCat} alt="light-cat" />
+        <div onClick={() => {setTheme('light')}}>
+          <div className={theme === 'light' ? style.light__button__large : style.light__button}>
+            <Image src={lightCat} alt="light-cat" width={theme === 'light' ? '156' : '104'} height={theme === 'light' ? '117' : '78'} />
           </div>
           <p className={style.light__text}>Light</p>
         </div>
 
-        <div>
-          <div className={style.dark__button}>
-            <Image src={darkCat} alt="dark-cat" />
+        <div onClick={() => {setTheme('dark')}}>
+          <div className={theme === 'dark' ? style.dark__button__large : style.dark__button}>
+            <Image src={darkCat} alt="dark-cat" width={theme === 'dark' ? '156' : '104'} height={theme === 'dark' ? '117' : '78'} />
           </div>
           <p className={style.dark__text}>Dark</p>
         </div>
 
-        <div>
-          <div className={style.system__button}>
+        {/* <div onClick={() => {setTheme('auto')}}>
+          <div className={theme === 'auto' ? style.system__button__large : style.system__button}>
             <div className={style.system__button__left}>
-              <Image src={lightCat} alt="light-cat" />
+              <Image src={lightCat} alt="light-cat" width={theme === 'auto' ? '156' : '104'} height={theme === 'auto' ? '117' : '78'} />
             </div>
             <div className={style.system__button__right}>
-              <Image src={halfCat} alt="half-cat" />
+              <Image src={halfCat} alt="half-cat" width={theme === 'auto' ? '156' : '100'} height={theme === 'auto' ? '117' : '129'} />
             </div>
+          </div>
+          <p className={style.light__text}>Auto</p>
+        </div> */}
+        <div onClick={() => {setTheme('auto')}}>
+          <div className={theme === 'auto' ? style.system__button__large : style.system__button}>
+            <Image src={lightCat} alt="light-cat" width={theme === 'auto' ? '156' : '104'} height={theme === 'auto' ? '117' : '78'} />
+            {/* <div className={style.system__button__left}>
+              <Image src={lightCat} alt="light-cat" width={theme === 'auto' ? '156' : '104'} height={theme === 'auto' ? '117' : '78'} />
+            </div>
+            <div className={style.system__button__right}>
+              <Image src={halfCat} alt="half-cat" width={theme === 'auto' ? '156' : '100'} height={theme === 'auto' ? '117' : '129'} />
+            </div> */}
           </div>
           <p className={style.light__text}>Auto</p>
         </div>
